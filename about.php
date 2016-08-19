266c6cdeb5da762354d84aae7789f5e21d94acd4
Jon Prentice <jon77p@gmail.com>
2016-08-18 21:47:21 -0700

    Synced Website from Server

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php include("template/header.php"); ?>
+		<div class="container">
+		 	<h1>About Me</h1>
+		</div>
+		<div class="container">
+			<h2>[about page]</h2>
+		</div>
+<?php include("template/footer.php"); ?>
