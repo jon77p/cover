6843d1054bd3abf2ecba66f4dfc80529c55a0ed3
Jon Prentice <jon77p@gmail.com>
2016-11-18 08:37:06 -0800

    Updated music page and reinstalled nodejs

--- template/nav.php
+++ template/nav.php
@@ -1,16 +1,7 @@
                    <nav>
                       <ul class="nav masthead-nav">
                         <li><a href="cover">Home</a></li>
-                        <div class="dropdown nav navbar-nav">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects</a><span class="caret"></span>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenu">
-                                <li class="dropdown-header">Projects</li>
-                                <li><a href="#">Programming</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="music">Music</a></li>
-                            </ul>
-                        </div>
-                        <li><a onclick="myFunction()">Projects</a></li>
+                        <li><a href="music">Music</a></li>
                         <li><a href="about">About</a></li>
                         <li><a href="contact">Contact</a></li>
                         <li><a href="social">Social</a></li>
